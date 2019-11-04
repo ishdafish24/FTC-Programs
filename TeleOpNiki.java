4a7c63c1d9f4a38acf67d3ea95445be02abddbc8
ishdafish24 <51826348+ishdafish24@users.noreply.github.com>
2019-11-04 20:40:03 +0000

    TeleOp for Niki Drive Chain (see description)
    This is a TeleOp for the Niki Drive Chain.
    This program allows for the Niki Bot to be fully controlled by the robot controller.
    This program does not have the code for intake output mechanisms in it as the plans are still being finalized

---
+++ TeleOpNiki.java
@@ -0,0 +1,77 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
+@Disabled
+
+public class TeleOpNiki extends LinearOpMode {
+
+    // Map hardware to here.
+    NikiHardware robot = new NikiHardware();
+
+    @Override
+    public void runOpMode() {
+
+        telemetry.addData("Status", "Ready to Go!");
+        telemetry.update();
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+
+        robot.init(hardwareMap);
+        ElapsedTime runtime = new ElapsedTime();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // Setup a variable for each drive wheel to save power level for telemetry
+            double leftPower;
+            double rightPower;
+
+
+            // POV Mode uses left stick to go forward, and right stick to turn.
+            // - This uses basic math to combine motions and is easier to drive straight.
+
+
+            double drive = -gamepad1.left_stick_y;
+            double turn  =  gamepad1.right_stick_x;
+            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+
+
+            // Send calculated power to wheels
+            robot.leftDrive.setPower(leftPower); // M1 AND M3 for left & Right
+            robot.rightDrive.setPower(rightPower); // M2 AND M4 for Front & Back
+
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+            telemetry.update();
+        }
+    }
+}
